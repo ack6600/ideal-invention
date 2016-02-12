5f65286fdfa90fa2622eec675c60b30977c58b0b
Adam Urish <adamurish@gmail.com>
2016-02-11 19:33:05 -0600

    added barebones code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+import main.GameWrapper;
+import main.RenderEngine;
+import world.Tickable;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Adam Urish on 2/11/16.
  */
 public class Main
 {
-
+    public static void main(String[] args)
+    {
+        JPanel jPanel = new JPanel();
+        JFrame jFrame = new JFrame();
+        jPanel.add(jFrame);
+        jPanel.setSize(300,300);
+        jPanel.setVisible(true);
+        RenderEngine renderEngine = null;
+        try
+        {
+            renderEngine = new RenderEngine(jPanel,60,new File("src/output/"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        GameWrapper gameWrapper = new GameWrapper(renderEngine);
+    }
 
 }
